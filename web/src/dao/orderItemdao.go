d7eac6be5a2b81f4174c03881e7d6038f4cadc15
yangzx <1024276406@qq.com>
2021-05-05 14:14:20 +0800

    订单的管理

--- web/src/dao/orderItemdao.go
+++ web/src/dao/orderItemdao.go
@@ -5,17 +5,7 @@ import (
 	"utils"
 )
 
-//AddOrderItem 向数据库中插入订单项
-func AddOrderItem(orderItem *model.OrderItem) error {
-	//写sql语句
-	sql := "insert into order_items(count,amount,title,author,price,img_path,order_id) values(?,?,?,?,?,?,?)"
-	//执行
-	_, err := utils.Db.Exec(sql, orderItem.Count, orderItem.Amount, orderItem.Title, orderItem.Author, orderItem.Price, orderItem.ImgPath, orderItem.OrderID)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+
 
 //GetOrderItemsByOrderID 根据订单号获取该订单的所有订单项
 func GetOrderItemsByOrderID(orderID string) ([]*model.OrderItem, error) {
@@ -34,4 +24,16 @@ func GetOrderItemsByOrderID(orderID string) ([]*model.OrderItem, error) {
 		orderItems = append(orderItems, orderItem)
 	}
 	return orderItems, nil
+}
+
+//AddOrderItem 向数据库中插入订单项
+func AddOrderItem(orderItem *model.OrderItem) error {
+	//写sql语句
+	sql := "insert into order_items(count,amount,title,author,price,img_path,order_id) values(?,?,?,?,?,?,?)"
+	//执行
+	_, err := utils.Db.Exec(sql, orderItem.Count, orderItem.Amount, orderItem.Title, orderItem.Author, orderItem.Price, orderItem.ImgPath, orderItem.OrderID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
