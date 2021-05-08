7a61d419f9acce628990c179d782c485d41d496d
yangzx <1024276406@qq.com>
2021-05-08 13:15:32 +0800

    添加购物项

--- web/src/model/cartItem.go
+++ web/src/model/cartItem.go
@@ -1,5 +1,7 @@
 package model
 
+
+
 //CartItem 购物项结构体
 type CartItem struct {
 	CartItemID int64   //购物项的id
@@ -8,6 +10,7 @@ type CartItem struct {
 	Amount     float64 //购物项中图书的金额小计，通过计算得到
 	CartID     string  //当前购物项属于哪一个购物车
 }
+
 
 //GetAmount 获取购物项中图书的金额小计，有图书的价格和图书的数量计算得到
 func (cartItem *CartItem) GetAmount() float64 {
